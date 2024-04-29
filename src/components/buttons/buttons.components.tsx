ccce7e8566bd5467d562a80e232aac8179179f29
Dhei-vid <djetuk@gmail.com>
2024-04-29 10:30:25 +0100

    added necessary components - button, taskCard

--- src/components/buttons/buttons.components.tsx
+++ src/components/buttons/buttons.components.tsx
@@ -1,9 +1,42 @@
-interface IButtons {}
+import "./buttons.styles.scss";
 
-const Buttons: React.FC<IButtons> = () => {
+interface IButtons {
+  isEdit: boolean;
+  onClickEdit: () => void;
+  isSave: boolean;
+  onClickSave?: () => void;
+  isDelete: boolean;
+  onClickDelete: () => void;
+  isAdd: boolean;
+  onClickAdd: () => void;
+}
+
+const Buttons: React.FC<IButtons> = ({ isEdit, isAdd, isDelete, isSave }) => {
   return (
     <div>
-      <div></div>
+      {isEdit && (
+        <div className={"edit"}>
+          <p>Edit</p>
+        </div>
+      )}
+
+      {isAdd && (
+        <div className={"add"}>
+          <div></div>
+        </div>
+      )}
+
+      {isDelete && (
+        <div className={"delete"}>
+          <p>Delete</p>
+        </div>
+      )}
+
+      {isSave && (
+        <div className={"save"}>
+          <p>Save</p>
+        </div>
+      )}
     </div>
   );
 };
---
+++ src/components/taskCard/taskCard.component.tsx
@@ -0,0 +1,11 @@
+import "./taskCard.styles.scss";
+
+const TaskCard = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default TaskCard;
